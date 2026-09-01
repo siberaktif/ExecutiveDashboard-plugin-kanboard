0d25a3ef636f845c8506286ac4a3116b0b1bea86
Sencar Tosun <desendijital@gmail.com>
2026-09-01 22:32:36 +0300

    fix: sidebar içindeki user_id atamaları userSession-   >getId() ile değiştirilerek TodoNotes çakışması ve Undefined Variable   hatası onarıldı

--- Template/dashboard/sidebar.php
+++ Template/dashboard/sidebar.php
@@ -9,16 +9,16 @@
 
         <!-- 2. Orijinal Kanboard Menüleri -->
         <li <?= $this->app->checkMenuSelection('DashboardController', 'show') ? 'class="active"' : '' ?>>
-            <?= $this->url->link(t('Overview'), 'DashboardController', 'show', ['user_id' => $user['id']]) ?>
+            <?= $this->url->link(t('Overview'), 'DashboardController', 'show', ['user_id' => $this->userSession->getId()]) ?>
         </li>
         <li <?= $this->app->checkMenuSelection('DashboardController', 'projects') ? 'class="active"' : '' ?>>
-            <?= $this->url->link(t('My projects'), 'DashboardController', 'projects', ['user_id' => $user['id']]) ?>
+            <?= $this->url->link(t('My projects'), 'DashboardController', 'projects', ['user_id' => $this->userSession->getId()]) ?>
         </li>
         <li <?= $this->app->checkMenuSelection('DashboardController', 'tasks') ? 'class="active"' : '' ?>>
-            <?= $this->url->link(t('My tasks'), 'DashboardController', 'tasks', ['user_id' => $user['id']]) ?>
+            <?= $this->url->link(t('My tasks'), 'DashboardController', 'tasks', ['user_id' => $this->userSession->getId()]) ?>
         </li>
         <li <?= $this->app->checkMenuSelection('DashboardController', 'subtasks') ? 'class="active"' : '' ?>>
-            <?= $this->url->link(t('My subtasks'), 'DashboardController', 'subtasks', ['user_id' => $user['id']]) ?>
+            <?= $this->url->link(t('My subtasks'), 'DashboardController', 'subtasks', ['user_id' => $this->userSession->getId()]) ?>
         </li>
 
         <!-- 3. KRİTİK KOD: Diğer Tüm Eklentilerin (P1-P5, TodoNotes) Menülerini Geri Getiren Hook -->
