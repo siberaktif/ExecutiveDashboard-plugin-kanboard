1c00052d11b2b15c4a6f19e227a3cf7f1aa8a645
Sencar Tosun <desendijital@gmail.com>
2026-09-02 04:54:35 +0300

    fix: Tarayici guvenlik (CSP) engeline takilan dis kaynakli Vis.js kutuphanesi projeye dahil edildi (Asset/js/vis-network.min.js) ve yerel olarak yuklenmesi saglandi

--- Template/dashboard/overview.php
+++ Template/dashboard/overview.php
@@ -208,7 +208,7 @@
                     <!-- VIS.JS Container -->
                     <div id="mcc-relationgraph-container"></div>
                     
-                    <script type="text/javascript" src="https://unpkg.com/vis-network/standalone/umd/vis-network.min.js"></script>
+                    <script type="text/javascript" src="<?= $this->url->dir() ?>plugins/ExecutiveDashboard/Asset/js/vis-network.min.js"></script>
                     <script type="text/javascript">
                         document.addEventListener("DOMContentLoaded", function() {
                             var container = document.getElementById('mcc-relationgraph-container');
