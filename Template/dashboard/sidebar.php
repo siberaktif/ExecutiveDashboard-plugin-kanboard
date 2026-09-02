56774b9caf447bb44564609e4127bf82c430d23f
Sencar Tosun <desendijital@gmail.com>
2026-09-02 03:36:59 +0300

    fix: Sol menu siralamasi ve ikonlar istenilen hiyerarsiye gore duzeltildi

--- Template/dashboard/sidebar.php
+++ Template/dashboard/sidebar.php
@@ -1,31 +1,38 @@
 <div class="sidebar">
     <ul class="no-bullets" style="display:flex; flex-direction:column;">
-        <!-- 1. En üstte Yönetici Kontrol Merkezi -->
+        <!-- 1. Yönetici Kontrol Merkezi -->
         <li style="order: 1;" <?= $this->app->checkMenuSelection('ExecutiveDashboardController', 'index', 'ExecutiveDashboard') ? 'class="active"' : '' ?>>
             <?= $this->url->link('<i class="fa fa-briefcase fa-fw" aria-hidden="true"></i> <strong>' . t('Yönetici Kontrol Merkezi') . '</strong>', 'ExecutiveDashboardController', 'index', ['plugin' => 'ExecutiveDashboard']) ?>
         </li>
-        
-        <!-- 2. TodoNotes (Notlarım) Sekmesi (Çökme Korumalı Güvenli $user Kapsamı) -->
-        <?php $safe_user_id = isset($user) ? $user['id'] : $this->user->getId(); ?>
-        <li style="order: 2;" <?= $this->app->checkMenuSelection('TodoNotesController', 'ShowDashboard', 'TodoNotes') ? 'class="active"' : '' ?>>
-            <?= $this->url->link('<i class="fa fa-wpforms fa-fw" aria-hidden="true"></i> ' . t('Notlarım'), 'TodoNotesController', 'ShowDashboard', ['plugin' => 'TodoNotes', 'user_id' => $safe_user_id]) ?>
-        </li>
 
-        <!-- 3. Orijinal Kanboard Menüleri -->
-        <li style="order: 3;" <?= $this->app->checkMenuSelection('DashboardController', 'show') ? 'class="active"' : '' ?>>
-            <?= $this->url->link('<i class="fa fa-dashboard fa-fw" aria-hidden="true"></i> ' . t('Overview'), 'DashboardController', 'show', ['user_id' => $safe_user_id]) ?>
+        <?php $safe_user_id = isset($user) ? $user['id'] : $this->user->getId(); ?>
+        
+        <!-- 2. Özet Görünüm -->
+        <li style="order: 2;" <?= $this->app->checkMenuSelection('DashboardController', 'show') ? 'class="active"' : '' ?>>
+            <?= $this->url->link('<i class="fa fa-tachometer fa-fw" aria-hidden="true"></i> ' . t('Overview'), 'DashboardController', 'show', ['user_id' => $safe_user_id]) ?>
         </li>
-        <li style="order: 4;" <?= $this->app->checkMenuSelection('DashboardController', 'projects') ? 'class="active"' : '' ?>>
+        
+        <!-- 3. Projelerim -->
+        <li style="order: 3;" <?= $this->app->checkMenuSelection('DashboardController', 'projects') ? 'class="active"' : '' ?>>
             <?= $this->url->link('<i class="fa fa-folder fa-fw" aria-hidden="true"></i> ' . t('My projects'), 'DashboardController', 'projects', ['user_id' => $safe_user_id]) ?>
         </li>
-        <li style="order: 5;" <?= $this->app->checkMenuSelection('DashboardController', 'tasks') ? 'class="active"' : '' ?>>
+        
+        <!-- 4. Görevlerim -->
+        <li style="order: 4;" <?= $this->app->checkMenuSelection('DashboardController', 'tasks') ? 'class="active"' : '' ?>>
             <?= $this->url->link('<i class="fa fa-tasks fa-fw" aria-hidden="true"></i> ' . t('My tasks'), 'DashboardController', 'tasks', ['user_id' => $safe_user_id]) ?>
         </li>
-        <li style="order: 6;" <?= $this->app->checkMenuSelection('DashboardController', 'subtasks') ? 'class="active"' : '' ?>>
+        
+        <!-- 5. Altgörevlerim -->
+        <li style="order: 5;" <?= $this->app->checkMenuSelection('DashboardController', 'subtasks') ? 'class="active"' : '' ?>>
             <?= $this->url->link('<i class="fa fa-sitemap fa-fw" aria-hidden="true"></i> ' . t('My subtasks'), 'DashboardController', 'subtasks', ['user_id' => $safe_user_id]) ?>
         </li>
 
-        <!-- 4. Diğer Eklentilerin Kancaları (En Alta Gider) -->
+        <!-- 6. TodoNotes (Notlarım) -->
+        <li style="order: 6;" <?= $this->app->checkMenuSelection('TodoNotesController', 'ShowDashboard', 'TodoNotes') ? 'class="active"' : '' ?>>
+            <?= $this->url->link('<i class="fa fa-wpforms fa-fw" aria-hidden="true"></i> ' . t('Notlarım'), 'TodoNotesController', 'ShowDashboard', ['plugin' => 'TodoNotes', 'user_id' => $safe_user_id]) ?>
+        </li>
+
+        <!-- 7. Diğer Eklentilerin Kancaları (En Alta Gider) -->
         <div style="order: 7; display:contents;">
             <?= $this->hook->render('template:dashboard:sidebar', array('user' => isset($user) ? $user : array('id' => $safe_user_id))) ?>
         </div>
