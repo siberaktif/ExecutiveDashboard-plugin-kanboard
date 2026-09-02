313094346b929337d2f38e3bc427b0db176d0c09
Sencar Tosun <desendijital@gmail.com>
2026-09-02 19:13:44 +0300

    fix: Ust acilir menu ve sol menudeki active bold (kalin) font stili ezildi, TodoNotes ve AgileIndicators eklentilerine karsi evrensel JS siralayici (MCC > Notlarim > AgileIndicators) eklendi

--- Template/dashboard/header_menu.php
+++ Template/dashboard/header_menu.php
@@ -1,3 +1,36 @@
-<li <?= $this->app->checkMenuSelection('ExecutiveDashboardController', 'index', 'ExecutiveDashboard') ? 'class="active"' : '' ?>>
-    <?= $this->url->icon('briefcase', t('Yönetici Kontrol Merkezi'), 'ExecutiveDashboardController', 'index', ['plugin' => 'ExecutiveDashboard']) ?>
+<li class="mcc-menu-item" <?= $this->app->checkMenuSelection('ExecutiveDashboardController', 'index', 'ExecutiveDashboard') ? 'class="active"' : '' ?>>
+    <?= $this->url->icon('briefcase', '<span style="font-weight: normal !important;">' . t('Yönetici Kontrol Merkezi') . '</span>', 'ExecutiveDashboardController', 'index', ['plugin' => 'ExecutiveDashboard']) ?>
 </li>
+<script>
+(function() {
+    try {
+        var mccs = document.querySelectorAll('.mcc-menu-item');
+        mccs.forEach(function(mcc) {
+            var ul = mcc.parentNode;
+            if (ul) {
+                var agile = null, todo = null;
+                var lis = ul.children;
+                for (var i = 0; i < lis.length; i++) {
+                    var txt = lis[i].textContent || lis[i].innerText;
+                    if (txt.indexOf('AgileIndicators') !== -1 || txt.indexOf('Agile') === 0) agile = lis[i];
+                    if (txt.indexOf('Notlarım') !== -1 || txt.indexOf('TodoNotes') !== -1) todo = lis[i];
+                }
+                
+                // If it's the sidebar, lock MCC under the 4 core items
+                if (ul.parentNode && (ul.parentNode.classList.contains('sidebar') || ul.classList.contains('sidebar'))) {
+                    if (lis.length >= 5) {
+                        ul.insertBefore(mcc, ul.children[4]);
+                    }
+                }
+                
+                // Relative Sorting for Dropdowns/Headers: MCC -> TodoNotes -> AgileIndicators
+                if (todo) { ul.insertBefore(mcc, todo); }
+                if (agile) {
+                    if (todo) { ul.insertBefore(agile, todo.nextSibling); }
+                    else { ul.insertBefore(agile, mcc.nextSibling); }
+                }
+            }
+        });
+    } catch(e) {}
+})();
+</script>
--- Template/dashboard/sidebar.php
+++ Template/dashboard/sidebar.php
@@ -1,20 +1,32 @@
-<li id="mcc-sidebar-item" <?= $this->app->checkMenuSelection('ExecutiveDashboardController', 'index', 'ExecutiveDashboard') ? 'class="active"' : '' ?>>
-    <?= $this->url->link('<i class="fa fa-briefcase fa-fw" aria-hidden="true"></i> ' . t('Yönetici Kontrol Merkezi'), 'ExecutiveDashboardController', 'index', ['plugin' => 'ExecutiveDashboard']) ?>
+<li class="mcc-menu-item" <?= $this->app->checkMenuSelection('ExecutiveDashboardController', 'index', 'ExecutiveDashboard') ? 'class="active"' : '' ?>>
+    <?= $this->url->link('<i class="fa fa-briefcase fa-fw" aria-hidden="true"></i> <span style="font-weight: normal !important;">' . t('Yönetici Kontrol Merkezi') . '</span>', 'ExecutiveDashboardController', 'index', ['plugin' => 'ExecutiveDashboard']) ?>
 </li>
 <script>
 (function() {
     try {
-        var mcc = document.getElementById('mcc-sidebar-item');
-        if (mcc && mcc.parentNode) {
+        var mccs = document.querySelectorAll('.mcc-menu-item');
+        mccs.forEach(function(mcc) {
             var ul = mcc.parentNode;
-            var lis = ul.querySelectorAll('li');
-            // Kanboard core has exactly 4 items before plugins (Overview, My projects, My tasks, My subtasks)
-            // If there are at least 5 items, we want mcc to be the 5th item (index 4).
-            // This pushes AgileIndicators and others down.
-            if (lis.length >= 5) {
-                ul.insertBefore(mcc, lis[4]);
+            if (ul) {
+                var agile = null, todo = null;
+                var lis = ul.children;
+                for (var i = 0; i < lis.length; i++) {
+                    var txt = lis[i].textContent || lis[i].innerText;
+                    if (txt.indexOf('AgileIndicators') !== -1 || txt.indexOf('Agile') === 0) agile = lis[i];
+                    if (txt.indexOf('Notlarım') !== -1 || txt.indexOf('TodoNotes') !== -1) todo = lis[i];
+                }
+                
+                if (ul.parentNode && (ul.parentNode.classList.contains('sidebar') || ul.classList.contains('sidebar'))) {
+                    if (lis.length >= 5) { ul.insertBefore(mcc, ul.children[4]); }
+                }
+                
+                if (todo) { ul.insertBefore(mcc, todo); }
+                if (agile) {
+                    if (todo) { ul.insertBefore(agile, todo.nextSibling); }
+                    else { ul.insertBefore(agile, mcc.nextSibling); }
+                }
             }
-        }
+        });
     } catch(e) {}
 })();
 </script>
